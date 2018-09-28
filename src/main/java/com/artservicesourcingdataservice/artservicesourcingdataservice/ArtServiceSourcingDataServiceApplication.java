68fa02546de661b51d4ef7d4b585ea81c51e1224
James Collerton <James.Collerton@kobaltmusic.com>
2018-09-28 16:21:31 +0100

    Made to work with the manipulating data service

--- src/main/java/com/artservicesourcingdataservice/artservicesourcingdataservice/ArtServiceSourcingDataServiceApplication.java
+++ src/main/java/com/artservicesourcingdataservice/artservicesourcingdataservice/ArtServiceSourcingDataServiceApplication.java
@@ -5,6 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+/*
+TODO: Comments
+TODO: Swagger
+TODO: Testing
+TODO: Docker
+ */
 @SpringBootApplication
 @EnableEurekaClient
 public class ArtServiceSourcingDataServiceApplication {
