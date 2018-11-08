fc1f7b05cb22ed8112c8c68f25cfc812431b5546
James Collerton <James.Collerton@kobaltmusic.com>
2018-11-08 06:34:34 -0800

    Tried to rewrite contract as a yaml file

--- src/test/java/com/artservicesourcingdataservice/artservicesourcingdataservice/contracts/BaseClass.java
+++ src/test/java/com/artservicesourcingdataservice/artservicesourcingdataservice/contracts/BaseClass.java
@@ -1,4 +1,4 @@
-package com.artservicesourcingdataservice.artservicesourcingdataservice.contracts.pieceinformation;
+package com.artservicesourcingdataservice.artservicesourcingdataservice.contracts;
 
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
